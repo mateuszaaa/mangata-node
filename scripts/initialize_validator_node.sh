81947f1049056c12b62bfa967c8c776a44dd1564
Jakub Sycha <jakub.sycha@ixperta.com>
2021-10-08 13:05:30 +0200

    Add dockerfile for validator node

--- scripts/initialize_validator_node.sh
+++ scripts/initialize_validator_node.sh
@@ -1,20 +1,43 @@
 #!/usr/bin/env bash
 
-/mangata/node &
+set -x
+set -e
+
+mkdir -p /etc/mangata/keys
+
+echo "=============================="
+echo "====> Node command:"
+echo "/mangata/node \
+        --chain $NODE_CHAIN \
+        --unsafe-ws-external \
+        --validator \
+        --rpc-methods Unsafe \
+        --rpc-cors all \
+        --name $NODE_NAME $NODE_PARAMS"
+echo "=============================="
+
+/mangata/node \
+  --chain "$NODE_CHAIN" \
+  --unsafe-ws-external \
+  --validator \
+  --rpc-methods "Unsafe" \
+  --rpc-cors "all" \
+  --name "$NODE_NAME" $NODE_PARAMS &
+
 node_pid=$!
 
 ready=false
 
 function generateKeys() {
   echo "====> Generating keys..."
-  cd /etc/mangata/keys && /usr/bin/node-keygen generate --plaintext --filename validator
+  cd /etc/mangata/keys && /usr/bin/node-keygen generate --plaintext --filename validator --subkeycommand "/usr/bin/subkey"
   echo "====> Generated keys:"
   cd /etc/mangata/keys && cat keys.txt
 }
 
 function injectKeys() {
   echo "====> Injecting keys..."
-  cd /etc/mangata/keys && /usr/bin/node-keygen inject --filename validator --injectionEndpoint "http://127.0.0.1:9933"
+  cd /etc/mangata/keys && /usr/bin/node-keygen inject --filename validator --injectionEndpoint "http://127.0.0.1:9933" --subkeycommand "/usr/bin/subkey"
 }
 
 function restartNode() {
@@ -28,13 +51,13 @@ function restartNode() {
   --validator \
   --rpc-methods "Unsafe" \
   --rpc-cors "all" \
-  --name "$NODE_NAME"
+  --name "$NODE_NAME" $NODE_PARAMS
 }
 
 while [ "$ready" = "false" ]; do
-  node_status=$(curl --location --request POST 'http://127.0.0.1:9933' --header 'Content-Type: application/json' --data-raw '{"id": 999, "jsonrpc": "2.0", "method": "system_health"}')
+  node_status=$(curl --location --request POST 'http://127.0.0.1:9933' --header 'Content-Type: application/json' --data-raw '{"id": 999, "jsonrpc": "2.0", "method": "system_health"}' | jq .result.isSyncing )
 
-  if [ "$node_status" = "true" ]; then
+  if [ "$node_status" = "false" ]; then
     ready=true
     echo "====> Node is ready, generating and injecting keys."
     generateKeys

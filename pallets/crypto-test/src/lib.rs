9bb039c794ed265c46bf0c901969d3a4494fea9b
Striker <siddiquishoeb.iitm@gmail.com>
2021-10-29 19:07:06 +0530

    Key capsulation, as in ecies, working

--- pallets/crypto-test/src/lib.rs
+++ pallets/crypto-test/src/lib.rs
@@ -6,9 +6,15 @@ use frame_support::pallet_prelude::*;
 use frame_system::pallet_prelude::*;
 use sp_std::prelude::*;
 
+use secp256k1::{SecretKey, PublicKey, Error as SecpError, util::FULL_PUBLIC_KEY_SIZE};
+use hkdf::Hkdf;
+use sha2::Sha256;
+
 use aes_gcm::aead::{generic_array::GenericArray, AeadInPlace, NewAead};
 use aes_gcm::{Aes256Gcm};
 
+/// Type alias for `[u8; 32]`, which is a 256-bit key
+pub type AesKey = [u8; 32];
 /// AES IV/nonce length
 pub const AES_IV_LENGTH: usize = 12;
 /// AES tag length
@@ -56,7 +62,10 @@ pub mod pallet {
     pub enum Error<T> {
         EncryptionFailed,
         DecryptionFailed,
-        CheckFailed
+        CheckFailed,
+        SecretKeyParseFailed,
+        EncapsulationFailed,
+        DecapsulationFailed
     }
 
 
@@ -78,11 +87,20 @@ pub mod pallet {
 
             let msg: &[u8] = b"hello!!";
 
-            let key: &[u8] = b"00010001010101110001000101010111";
+            let secret_key_seed_1: [u8; 32] = *b"00010001010101110001000101010111";
+            let secret_key_seed_2: [u8; 32] = *b"00010001010101110001000101011111";
 
-            let key = GenericArray::from_slice(key);
+            let secret_key_1: SecretKey = SecretKey::parse(&secret_key_seed_1).map_err(|_| Error::<T>::SecretKeyParseFailed)?;
+            let secret_key_2: SecretKey = SecretKey::parse(&secret_key_seed_2).map_err(|_| Error::<T>::SecretKeyParseFailed)?;
 
-            let aead = Aes256Gcm::new(key);
+            let public_key_1: PublicKey = PublicKey::from_secret_key(&secret_key_1);
+            let public_key_2: PublicKey = PublicKey::from_secret_key(&secret_key_2);
+
+            let aes_key = Pallet::<T>::encapsulate(&secret_key_1, &public_key_2).map_err(|_| Error::<T>::EncapsulationFailed)?;
+
+            let aes_key = GenericArray::from_slice(&aes_key);
+
+            let aead = Aes256Gcm::new(aes_key);
 
             let iv = [0u8; AES_IV_LENGTH];
 
@@ -108,7 +126,11 @@ pub mod pallet {
 
             let encrypted_msg = output.clone();
 
-            let aead = Aes256Gcm::new(key);
+            let aes_key = Pallet::<T>::decapsulate(&public_key_1, &secret_key_2).map_err(|_| Error::<T>::DecapsulationFailed)?;
+
+            let aes_key = GenericArray::from_slice(&aes_key);
+
+            let aead = Aes256Gcm::new(aes_key);
 
             let iv_from_encrypted = GenericArray::from_slice(&encrypted_msg[..AES_IV_LENGTH]);
             let tag_from_encrypted = GenericArray::from_slice(&encrypted_msg[AES_IV_LENGTH..AES_IV_PLUS_TAG_LENGTH]);
@@ -136,4 +158,40 @@ pub mod pallet {
         }
 
     }
+}
+
+impl<T: Config> Pallet<T> {
+    /// Calculate a shared AES key of our secret key and peer's public key by hkdf
+    pub fn encapsulate(sk: &SecretKey, peer_pk: &PublicKey) -> Result<AesKey, SecpError> {
+        let mut shared_point = peer_pk.clone();
+        shared_point.tweak_mul_assign(&sk)?;
+
+        let mut master = Vec::with_capacity(FULL_PUBLIC_KEY_SIZE * 2);
+        master.extend(PublicKey::from_secret_key(&sk).serialize().iter());
+        master.extend(shared_point.serialize().iter());
+
+        Self::hkdf_sha256(master.as_slice())
+    }
+
+    /// Calculate a shared AES key of our public key and peer's secret key by hkdf
+    pub fn decapsulate(pk: &PublicKey, peer_sk: &SecretKey) -> Result<AesKey, SecpError> {
+        let mut shared_point = pk.clone();
+        shared_point.tweak_mul_assign(&peer_sk)?;
+
+        let mut master = Vec::with_capacity(FULL_PUBLIC_KEY_SIZE * 2);
+        master.extend(pk.serialize().iter());
+        master.extend(shared_point.serialize().iter());
+
+        Self::hkdf_sha256(master.as_slice())
+    }
+
+    // private below
+    fn hkdf_sha256(master: &[u8]) -> Result<AesKey, SecpError> {
+        let h = Hkdf::<Sha256>::new(None, master);
+        let mut out = [0u8; 32];
+        h.expand(&EMPTY_BYTES, &mut out)
+            .map_err(|_| SecpError::InvalidInputLength)?;
+        Ok(out)
+    }
+
 }
